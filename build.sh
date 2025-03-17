33957d5e0820c701367aae727ebfe0f5d353ece5
texascake <budalblimbing@gmail.com>
2025-03-18 06:19:53 +0700

    Update trees

--- build.sh
+++ build.sh
@@ -206,7 +206,7 @@ DATE2=$(TZ=Asia/Jakarta date +"%d%m%Y-%H%M")
 # Function to replace defconfig versioning
 setversioning() {
     # For staging branch
-    KERNELNAME="TOM-$BUILD_TYPE-SUSFS-TEST-$LINUXVER"
+    KERNELNAME="TOM-$BUILD_TYPE-OC-$LINUXVER"
     # Export our new localversion and zipnames
     ZIPNAME="$KERNELNAME"
 }
