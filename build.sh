6b690a86de5d2ada079e7eceb9d52978b2268e95
texascake <tiktodz@soulvibe-team.cloud>
2025-04-17 12:38:57 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -223,7 +223,7 @@ DATE2=$(TZ=Asia/Jakarta date +"%d%m%Y-%H%M")
 # Function to replace defconfig versioning
 setversioning() {
     # For staging branch
-    KERNELNAME="$KERNAME-$BUILD_TYPE-$LINUXVER"
+    KERNELNAME="$KERNAME-$BUILD_TYPE-$LINUXVER-$DATE2"
     # Export our new localversion and zipnames
     ZIPNAME="$KERNELNAME"
 }
