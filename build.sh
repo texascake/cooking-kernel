5894cd9e491fb9a0bee73960bc818270b0c65500
texascake <tiktodz@soulvibe-team.cloud>
2025-08-08 10:40:34 +0700

    Update build

--- build.sh
+++ build.sh
@@ -47,10 +47,10 @@ cd $KERNEL_DIR
 sed -i 's/CONFIG_DEBUG_INFO=.*/CONFIG_DEBUG_INFO=n/g' arch/arm64/configs/asus/X00TD_defconfig
 sed -i 's/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION="-TOMTzy"/g' arch/arm64/configs/asus/X00TD_defconfig
 sed -i 's/CONFIG_HAVE_KPROBES=.*/CONFIG_HAVE_KPROBES=n/g' arch/arm64/configs/asus/X00TD_defconfig
-#sed -i 's/CONFIG_ARCH_HAS_REFCOUNT_FULL=.*/CONFIG_ARCH_HAS_REFCOUNT_FULL=n/g' arch/arm64/configs/asus/X00TD_defconfig
-#sed -i 's/CONFIG_ARCH_HAS_REFCOUNT=.*/CONFIG_ARCH_HAS_REFCOUNT=n/g' arch/arm64/configs/asus/X00TD_defconfig
-#sed -i 's/CONFIG_BLK_MQ_RDMA=.*/CONFIG_BLK_MQ_RDMA=n/g' arch/arm64/configs/asus/X00TD_defconfig
-#sed -i 's/CONFIG_BLK_PM=.*/CONFIG_BLK_PM=n/g' arch/arm64/configs/asus/X00TD_defconfig
+sed -i 's/CONFIG_ARCH_HAS_REFCOUNT_FULL=.*/CONFIG_ARCH_HAS_REFCOUNT_FULL=n/g' arch/arm64/configs/asus/X00TD_defconfig
+sed -i 's/CONFIG_ARCH_HAS_REFCOUNT=.*/CONFIG_ARCH_HAS_REFCOUNT=n/g' arch/arm64/configs/asus/X00TD_defconfig
+sed -i 's/CONFIG_BLK_MQ_RDMA=.*/CONFIG_BLK_MQ_RDMA=n/g' arch/arm64/configs/asus/X00TD_defconfig
+sed -i 's/CONFIG_BLK_PM=.*/CONFIG_BLK_PM=n/g' arch/arm64/configs/asus/X00TD_defconfig
 
 # The name of the device for which the kernel is built
 MODEL="Asus Zenfone Max Pro M1"
@@ -75,7 +75,7 @@ KERNAME=TOM
 KBUILD_BUILD_USER=Kneba
 
 # Build Type
-BUILD_TYPE=PreRelease
+BUILD_TYPE=DownRev
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc'
