fc964f9c844141e486a773e7bdaf12e72b6c571c
texascake <tiktodz@soulvibe-team.cloud>
2025-08-15 22:52:04 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -23,8 +23,8 @@ KERNELDIR=$(pwd)
 CODENAME="Hayzel"
 DEVICENAME="X00TD"
 KERNELNAME="TOM"
-VARIANT="HMP"
-VERSION="CLO"
+VARIANT="EAS"
+VERSION="CIP-st25"
 KERVER=$(make kernelversion)
 
 BONUS_MSG="*Note:* KernelSU-Next and APatch Supported!! 🤫"
@@ -248,7 +248,7 @@ fi
 echo "**** Verifying AnyKernel3 Directory ****"
 if ! [ -d "$KERNELDIR/AnyKernel3" ]; then
   echo "AnyKernel3 not found! Cloning..."
-  if ! git clone --depth=1 https://github.com/Kneba/AnyKernel3 -b 44-hmp AnyKernel3; then
+  if ! git clone --depth=1 https://github.com/texascake/AnyKernel3 AnyKernel3; then
     tg_post_build "$KERNELDIR/out/arch/arm64/boot/Image.gz-dtb" "Failed to Clone Anykernel, Sending image file instead"
     echo "Cloning failed! Aborting..."
     exit 1
