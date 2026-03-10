ddb282636e0f06b136394c48c72599449e6d7fd9
texascake <tiktodz@soulvibe-team.cloud>
2026-03-10 19:39:45 +0700

    Update build.sh 19.40

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ sed -i 's/CONFIG_DEBUG_INFO=.*/CONFIG_DEBUG_INFO=n/g' arch/arm64/configs/X00TD_d
 #sed -i 's/CONFIG_KSU=.*/CONFIG_KSU=n/g' arch/arm64/configs/X00TD_defconfig
 #sed -i 's/CONFIG_SCHED_WALT=.*/CONFIG_SCHED_WALT=n/g' arch/arm64/configs/X00TD_defconfig
 
-#curl -LSs "https://raw.githubusercontent.com/Sorayukii/KernelSU-Next/stable/kernel/setup.sh" | bash -s hookless
+curl -LSs "https://raw.githubusercontent.com/Sorayukii/KernelSU-Next/stable/kernel/setup.sh" | bash -s hookless
 
 # Set the Variables
 KERNELDIR=$(pwd)
@@ -30,7 +30,7 @@ VARIANT="EAS"
 VERSION="EOL"
 KERVER=$(make kernelversion)
 
-BONUS_MSG="*Note:* Use Magisk better!!"
+BONUS_MSG="*Note:* Jika itu memang terbaik!!"
 
 ## Set defaults first
 HOST=$(uname -a | awk '{print $2}')
