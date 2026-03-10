513a142f8e105c137e0b158d5a458bfd05b051a8
texascake <tiktodz@soulvibe-team.cloud>
2026-03-11 05:24:37 +0700

    Update build.sh 05.24

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ else
 fi
 
 # Additional command (if you're lazy to commit :v)
-sed -i 's/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION="-TOM-EAS-969+"/g' arch/arm64/configs/X00TD_defconfig
+sed -i 's/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION="-TOMxVoidWalker-EAS-969+"/g' arch/arm64/configs/X00TD_defconfig
 sed -i 's/CONFIG_DEBUG_INFO=.*/CONFIG_DEBUG_INFO=n/g' arch/arm64/configs/X00TD_defconfig
 #sed -i 's/CONFIG_KSU=.*/CONFIG_KSU=n/g' arch/arm64/configs/X00TD_defconfig
 #sed -i 's/CONFIG_SCHED_WALT=.*/CONFIG_SCHED_WALT=n/g' arch/arm64/configs/X00TD_defconfig
