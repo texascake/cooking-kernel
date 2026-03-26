1ae0c4e4dce17e48bef9f3bc1fd6014083da6e26
texascake <tiktodz@soulvibe-team.cloud>
2026-03-26 23:05:48 +0700

    Update to master

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ sed -i 's/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION="-TOM-HMP-969"/g' arch/arm6
 #sed -i '$aCONFIG_KSU=y' arch/arm64/configs/X00TD_defconfig
 #sed -i '$aCONFIG_KSU_TAMPER_SYSCALL_TABLE=y' arch/arm64/configs/X00TD_defconfig
 #curl -LSs "https://raw.githubusercontent.com/Sorayukii/KernelSU-Next/stable/kernel/setup.sh" | bash -s hookless
-curl -LSs "https://raw.githubusercontent.com/Kneba/KernelSU/master/kernel/setup.sh" | bash -s staging
+curl -LSs "https://raw.githubusercontent.com/Kneba/KernelSU/master/kernel/setup.sh" | bash -s master
 
 # Set the Variables
 KERNELDIR=$(pwd)
